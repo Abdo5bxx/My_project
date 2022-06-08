49cc9f8c6f6d5998712730da8c1a0013f14f821b
Abdullah Ali <abdullahtony555@gmail.com>
2022-06-08 16:55:48 +0200

    added

---
+++ calc.py
@@ -0,0 +1,27 @@
+calculator = True
+while calculator == True:
+    fir_num = float(input("Enter the first Number: "))
+    op = input("Enter the operator (+, -, *, /): ")
+    sec_num = float(input("Enter the second number: "))
+    
+    i = 0
+    while i < 1:
+        i = i + 1
+        if op == "+":
+            result = fir_num + sec_num
+            print(result)
+        elif op == "-":
+            result = fir_num - sec_num
+            print(result)
+        elif op == "*":
+            result = fir_num * sec_num
+            print(result)
+        elif op == "/":
+            result = fir_num / sec_num
+            print(result)
+        exit = input("Do you want to exit the program (y/n): ")
+        if exit == "y":
+            calculator = False
+        else:
+            calculator = True
+    
